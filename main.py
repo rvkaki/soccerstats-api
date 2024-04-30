b10fe9e43b4d615ae7e8a99606cb380ad9ac73fa
Ricardo Vieira <rjvmcv.98@gmail.com>
2024-04-30 16:15:52 +0100

    Update CORS origin to allow all origins in main.py

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ from constants import WorldCupSBId, WorldCupSBSeasonId, WorldCupFBRefId, WorldCu
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "https://soccerstats-ten.vercel.app/"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
